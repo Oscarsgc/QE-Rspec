f215e1317b1891112359c7f76707eecf55907644
Oscar Gonzalez <oscar_sgc@hotmail.com>
2015-10-28 17:52:52 -0400

    Specs

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,20 +3,53 @@ require 'spec_helper'
 
 describe Person do
 
-  before :each do
-    @person = Person.new("Oscar", "González", 22)
-  end
-
   describe "#new" do
-    it "takes three parameters and returns a Person object" do
-      @person.should be_an_instance_of Person
+    before do
+      @person = Person.initializer
+    end
+
+    it "empty constructor returns a Person object" do
+      expect(@person).to be_a Person
     end
   end
 
-  describe "#name" do
-    it "returns the correct name" do
-      @person.first_name.should eql "Oscarr"
+
+  describe "#first_name" do
+    before do
+      @person = Person.new("Oscar", "González", 22)
+    end
+    it "should return the correct name" do
+      expect(@person.first_name).to eql "Oscar"
     end
   end
+
+  describe "#last_name" do
+    before do
+      @person = Person.new("Oscar", "González", 22)
+    end
+    it "should return the correct name" do
+      expect(@person.last_name).to eql "González"
+    end
+  end
+
+  describe "#age" do
+    before do
+      @person = Person.new("Oscar", "González", 22)
+    end
+    it "should return the correct age" do
+      expect(@person.age).to eql 22
+    end
+  end
+
+  describe "#age" do
+    before do
+      @person = Person.new("Oscar", "González", 22)
+    end
+    it "should return the correct cash_balance" do
+      expect(@person.cash).to eql 100000
+    end
+  end
+
+
 
 end
