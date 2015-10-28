d657b975826bc008a8f3f306f7941843c46ccb58
Oscar Gonzalez <oscar_sgc@hotmail.com>
2015-10-28 17:12:50 -0400

    Magic

---
+++ insufficient_funds_exception.rb
@@ -0,0 +1,5 @@
+class InsufficientFundsException < Exception
+	def initialize
+		
+	end	
+end
---
+++ spec/person_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../person'
+require 'spec_helper'
+
+describe Person do
+
+  before :each do
+    @person = Person.new("Oscar", "González", 22)
+  end
+
+  describe "#new" do
+    it "takes three parameters and returns a Person object" do
+      @person.should be_an_instance_of Person
+    end
+  end
+
+  describe "#name" do
+    it "returns the correct name" do
+      @person.first_name.should eql "Oscarr"
+    end
+  end
+
+end
